ed25358614c9065b8d988510e1d76a93d95bce2b
DQNEO <dqneoo@gmail.com>
2014-11-10 16:12:43 +0900

    add document

--- parser.h
+++ parser.h
@@ -8,4 +8,9 @@ struct Credential
 };
 
 void free_credential(struct Credential *crd);
+
+/**
+ * parse json_string and set credential structure
+ * @return -1:error,  0:success
+ */
 int parse_credential(const char *json_string, struct Credential *crd);
