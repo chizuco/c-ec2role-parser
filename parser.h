dd146ce3d1752d0831adb6dd91ae654a547f1b40
DQNEO <dqneoo@gmail.com>
2014-11-10 16:10:21 +0900

    refactor

--- parser.h
+++ parser.h
@@ -5,6 +5,5 @@ struct Credential
     const char *secretaccesskey;
 };
 
-char *file_get_contents(const char *filename);
 void free_credential(struct Credential *crd);
 int parse_credential(const char *json_string, struct Credential *crd);
