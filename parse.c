ba29fba846cfddde20ec8ccebc2bf553df7dee7f
DQNEO <dqneoo@gmail.com>
2014-11-10 14:32:28 +0900

    refactor:extract function

--- parse.c
+++ parse.c
@@ -47,8 +47,26 @@ void free_credential(struct Credential *crd)
     free((void *)crd->accesskeyid);
     free((void *)crd->token);
     free((void *)crd->secretaccesskey);
+
+    crd->accesskeyid = NULL;
+    crd->token = NULL;
+    crd->secretaccesskey = NULL;
 }
 
+void parse_credential(const char *json_string, struct Credential *crd)
+{
+    struct json_object *obj = json_tokener_parse(json_string);
+
+    json_object_object_foreach(obj, key, val) {
+        if (strcmp(key, "AccessKeyId") == 0) {
+            crd->accesskeyid = json_object_to_json_string(val);
+        } else if (strcmp(key, "SecretAccessKey") == 0) {
+            crd->secretaccesskey = json_object_to_json_string(val);
+        } else if (strcmp(key, "Token") == 0) {
+            crd->token = json_object_to_json_string(val);
+        }
+    }
+}
 
 int main(int argc, char ** argv)
 {
@@ -60,24 +78,16 @@ int main(int argc, char ** argv)
     char *content = file_get_contents(argv[1]);
     printf("%s", content);
 
+
     struct Credential crd;
-
-    struct json_object *obj = json_tokener_parse(content);
-
-    json_object_object_foreach(obj, key, val) {
-        if (strcmp(key, "AccessKeyId") == 0) {
-            crd.accesskeyid = json_object_to_json_string(val);
-        } else if (strcmp(key, "SecretAccessKey") == 0) {
-            crd.secretaccesskey = json_object_to_json_string(val);
-        } else if (strcmp(key, "Token") == 0) {
-            crd.token = json_object_to_json_string(val);
-        }
-    }
+    parse_credential(content, &crd);
 
     printf("%s\n", crd.accesskeyid);
     printf("%s\n", crd.secretaccesskey);
     printf("%s\n", crd.token);
-    free(content);
+
     free_credential(&crd);
+    free(content);
+
     return 0;
 }
