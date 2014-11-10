6ec243975854cb5ec3d0bc91d4a0cc0349c03fdb
DQNEO <dqneoo@gmail.com>
2014-11-10 11:49:21 +0900

    refactor:extract function

--- parse.c
+++ parse.c
@@ -2,42 +2,46 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
-int main(int argc, char ** argv)
+char *file_get_contents(const char *filename)
 {
     FILE *fin;
-    char *filename;
-
-    if (argc < 2) {
-        fprintf(stderr, "no filename given\n");
-        return 1;
-    }
-
-    filename = argv[1];
     struct stat st;
 
     if (stat(filename, &st) < 0) {
         fprintf(stderr, "unable to stat %s\n", filename);
-        return 1;
+        return NULL;
     }
 
-    fin = fopen(argv[1], "r");
+    fin = fopen(filename, "r");
     if (fin == NULL) {
-        fprintf(stderr, "unable to open file %s", argv[1]);
-        return 1;
+        fprintf(stderr, "unable to open file %s", filename);
+        return NULL;
     }
 
     char *content;
 
     content = (char *) malloc(st.st_size);
-
     if (fread(content, st.st_size, 1, fin) == 0) {
-        fprintf(stderr, "unable to open file %s", argv[1]);
+        fprintf(stderr, "unable to open file %s", filename);
+        return NULL;
+    }
+
+    fclose(fin);
+
+    return content;
+}
+
+
+int main(int argc, char ** argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "no filename given\n");
         return 1;
     }
 
+    char *content = file_get_contents(argv[1]);
     printf("%s", content);
 
     free(content);
-    fclose(fin);
     return 0;
 }
