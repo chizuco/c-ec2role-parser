912eef65cfc2fc260d558ab73311f68cf651cf9d
DQNEO <dqneoo@gmail.com>
2014-11-10 16:06:35 +0900

    separate compliation

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include "parser.h"
+
+char *file_get_contents(const char *filename)
+{
+    FILE *fin;
+    struct stat st;
+
+    if (stat(filename, &st) < 0) {
+        fprintf(stderr, "unable to stat %s\n", filename);
+        return NULL;
+    }
+
+    fin = fopen(filename, "r");
+    if (fin == NULL) {
+        fprintf(stderr, "unable to open file %s", filename);
+        return NULL;
+    }
+
+    char *content;
+
+    content = (char *) malloc(st.st_size);
+    if (fread(content, st.st_size, 1, fin) == 0) {
+        fprintf(stderr, "unable to open file %s", filename);
+        return NULL;
+    }
+
+    fclose(fin);
+
+    return content;
+}
+
 
 int main(int argc, char ** argv)
 {
--- parser.c
+++ parser.c
@@ -1,39 +1,8 @@
 #include <stdio.h>
-#include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
 #include <json-c/json.h>
 #include "parser.h"
-
-char *file_get_contents(const char *filename)
-{
-    FILE *fin;
-    struct stat st;
-
-    if (stat(filename, &st) < 0) {
-        fprintf(stderr, "unable to stat %s\n", filename);
-        return NULL;
-    }
-
-    fin = fopen(filename, "r");
-    if (fin == NULL) {
-        fprintf(stderr, "unable to open file %s", filename);
-        return NULL;
-    }
-
-    char *content;
-
-    content = (char *) malloc(st.st_size);
-    if (fread(content, st.st_size, 1, fin) == 0) {
-        fprintf(stderr, "unable to open file %s", filename);
-        return NULL;
-    }
-
-    fclose(fin);
-
-    return content;
-}
-
 
 void free_credential(struct Credential *crd)
 {
